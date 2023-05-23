da0d02e52d05105e0f0e534a042ff3e88da972d1
Mukan Singh <mukeshschouhan111@gmail.com>
2023-05-23 10:19:13 +0530

    new project laravel

---
+++ resources/views/chouhan.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Chouhan</title>
+        <style>
+            .name{
+                display: flex;
+                width: 100%;
+                height: 100vh;
+                align-items: center;
+                justify-content: center;
+            }
+            h1{
+                color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="name">
+            <h1>MUKAN SINGH CHOUHAN</h1>
+        </div>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+route::get('/',function(){
+    return view('home');
+});
+route::get('/about',function(){
+    return view('aboutus');
+});
+route::get('/contact',function(){
+    return view('contactus');
+});
+route::get('/gallery',function(){
+    return view('gallery');
+});
+route::get('/enquiry',function(){
+    return view('enquiry');
+});
